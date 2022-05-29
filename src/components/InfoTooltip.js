99e78c25f830ee2bb08c3d5210bef9f3b4e6b24f
Dmitry Ilin <dm-ilin@mail.ru>
2022-05-29 12:20:22 +0400

    Исправить компонент InfoTooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,15 +1,15 @@
-import PopupWithForm from "./PopupWithForm";
 import regsuccess from "../images/regsuccess.svg";
 import regerror from "../images/regerror.svg";
 
 function InfoTooltip ({isOpen, onClose, isSuccessful}) {
   return(
-    <PopupWithForm containerClassName="popup__container_type_info" headingClassName="popup__heading_type_info" submitButtonHidden="popup__submit-button_hidden" isOpen={isOpen} onClose={onClose} >
-      <>
+    <section className={`popup ${isOpen && 'popup_opened'}`}>
+      <div className="popup__container popup__container_type_info">
         <img className="popup__pic" src={isSuccessful ? regsuccess : regerror}/>
         <h2 className="popup__heading popup__heading_type_info">{isSuccessful ? 'Вы успешно зарегистрировались!' : 'Что-то пошло не так! Попробуйте ещё раз.'} </h2>
-      </>
-    </PopupWithForm>
+        <button className="popup__close-button" type="button" onClick={onClose}/>
+      </div>
+    </section>
   )
 }
 
